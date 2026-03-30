b68bf5e98f906ce9f7ca0a11968f4999268c1fa8
Ajimi Mahdi <ajimimahdi1@gmail.com>
2026-03-30 13:45:49 +0200

    inserimento delle rotte crud collegandoli con i post sul file bacheca.js

--- index.js
+++ index.js
@@ -4,13 +4,47 @@ const port = 3000;
 
 
 app.use(express.static('public'));
+const listaPost = require('./blogPost/bacheca');
 
 app.get('/', (req, res) => {
   res.send('Server del mio blog');
 });
 
-
 // rotte crud 
+
+// index
+app.get('/bacheca', function (req, res) {
+res.json(listaPost);
+});
+
+
+// show
+app.get('/bacheca/:id', function (req, res) {
+res.send('Dettagli del post ' + req.params.id);
+});
+
+
+// store
+app.post('/bacheca', function (req, res) {
+res.send('Creazione nuovo post');
+});
+
+
+// update
+app.put('/bacheca/:id', function (req, res) {
+res.send('Modifica integrale del post ' + req.params.id);
+});
+
+
+// modify
+app.patch('/bacheca/:id', function (req, res) {
+res.send('Modifica parziale del post ' + req.params.id);
+});
+
+// destroy
+app.delete('/bacheca/:id', function (req, res) {
+res.send('Eliminazione del post ' + req.params.id);
+});
 
 
 
